f3d72b29d143184f2d3158d852aa86161172fba6
edwin001-tech <edwinshitakwa8@gmail.com>
2022-06-03 11:01:16 +0300

    first commit

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	        int length = 5;
+	        
+			int height = 10;
+			
+			int area = length * height;
+			
+			System.out.println("The area of the rectangle is = " + area);
+			}
+
+	}
+
+
